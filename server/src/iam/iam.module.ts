ff44c732ee3fa8ec8d071f0e86a8671abdf4aa9a
Artem Zaiarnyi <artemzaiarnyi@Artems-MacBook-Air.local>
2023-08-28 15:41:02 +0300

    add auth guards

--- server/src/iam/iam.module.ts
+++ server/src/iam/iam.module.ts
@@ -8,6 +8,9 @@ import { User } from 'src/users/entities/user.entity';
 import { JwtModule } from '@nestjs/jwt';
 import jwtConfig from './config/jwt.config';
 import { ConfigModule } from '@nestjs/config';
+import { APP_GUARD } from '@nestjs/core';
+import { AccessTokenGuard } from './authentication/guards/access-token/access-token.guard';
+import { AuthenticationGuard } from './authentication/guards/authentication/authentication.guard';
 
 @Module({
   imports: [
@@ -20,6 +23,11 @@ import { ConfigModule } from '@nestjs/config';
       provide: HashingService,
       useClass: BcryptService,
     },
+    {
+      provide: APP_GUARD,
+      useClass: AuthenticationGuard,
+    },
+    AccessTokenGuard,
     AuthenticationService,
   ],
   controllers: [AuthenticationController],
