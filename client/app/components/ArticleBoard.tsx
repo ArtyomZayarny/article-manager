f5fc295dcb481ce97a95a3f18fdde435c809ea20
Artem Zaiarnyi <artemzaiarnyi@Artems-MacBook-Air.local>
2023-09-15 18:00:04 +0300

    added empty view and fix artcle style

--- client/app/components/ArticleBoard.tsx
+++ client/app/components/ArticleBoard.tsx
@@ -1,23 +1,32 @@
-import { useContext, useEffect } from 'react';
-import { ArticlesContext } from '../context/article-context';
-import { Article } from './Article';
-import { Panel } from './Panel';
+import { useContext, useEffect, useMemo } from "react";
+import { ArticlesContext } from "../context/article-context";
+import { Article } from "./Article";
+import { Panel } from "./Panel";
+import { usePathname } from "next/navigation";
 
 export const ArticleBoard = () => {
-  const { storedArticles, loading,searchString, searchArticle} = useContext(ArticlesContext);
+  const { storedArticles, loading, searchString, searchArticle } =
+    useContext(ArticlesContext);
+  const pathname = usePathname();
+  const isAdminPage = useMemo(() => pathname.includes("admin"), [pathname]);
 
-  useEffect( () => {
-    if(searchString) {
-      searchArticle(searchString)
+  useEffect(() => {
+    if (searchString) {
+      searchArticle(searchString);
     }
-  },[searchString, ])
+  }, [searchString]);
 
   return (
-    <div>
-      <Panel />
-      <div className='flex justify-center'>
-        <div className='grid grid-cols-3 gap-4 w-full max-w-4xl py-6'>
-          {loading && <h2>Loading.......</h2>}
+    <div className={"mt-6"}>
+      {storedArticles.length > 0 && <Panel />}
+      <div className="flex justify-center">
+        {loading && <h2>Loading.......</h2>}
+        {isAdminPage ? (
+          <h4>Click plus button to add your first article</h4>
+        ) : (
+          <h2 className={"flex justify-center"}>There is no article to show</h2>
+        )}
+        <div className="grid grid-cols-3 gap-4 w-full max-w-4xl py-6">
           {storedArticles &&
             storedArticles.map((article) => (
               <Article key={article.id} article={article} />
--- client/app/components/Panel.tsx
+++ client/app/components/Panel.tsx
@@ -1,19 +1,22 @@
-import { Button } from '@mui/material';
-import { SearchBar } from './SearchBar';
-import { useCallback, useContext } from 'react';
-import { ArticlesContext } from '../context/article-context';
+import { Button } from "@mui/material";
+import { SearchBar } from "./SearchBar";
+import { useContext } from "react";
+import { ArticlesContext } from "../context/article-context";
 
 export const Panel = () => {
-  const {sortArticleByDate} = useContext(ArticlesContext)
-
-
-
+  const { sortArticleByDate } = useContext(ArticlesContext);
 
   return (
-    <div className={'flex justify-center'}>
-      <div className={'w-full max-w-4xl flex'}>
-      <SearchBar />
-      <Button onClick={sortArticleByDate} variant="outlined" style={{width:200, marginLeft:15}}>Sort by Date</Button>
+    <div className={"flex justify-center"}>
+      <div className={"w-full max-w-4xl flex"}>
+        <SearchBar />
+        <Button
+          onClick={sortArticleByDate}
+          variant="outlined"
+          style={{ width: 200, marginLeft: 15 }}
+        >
+          Sort by Date
+        </Button>
       </div>
     </div>
   );
